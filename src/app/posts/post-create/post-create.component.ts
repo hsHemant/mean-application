c4da22f2049b3e696b59fe851e27db875bc35904
shalini <shalinisoni2510@gmail.com>
2019-06-08 10:20:23 +0100

    Listening to Events

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+//decorator
 @Component({
     selector: 'app-post-create',
     templateUrl: './post-create.component.html'
 })
 
+//class
 export class PostCreateComponent {
+    onAddPost(){
+        alert('post added:');
+    }
 }
